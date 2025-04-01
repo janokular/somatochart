d4eb48d04314816289b0d2449f0042891bb266b7
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-04-01 14:51:44 +0200

    staged changes

--- somato-chart/client/src/app/app.routes.ts
+++ somato-chart/client/src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from "@angular/router";
 import { AthletesListComponent } from "./athletes-list/athletes-list.component";
 import { AddAthleteComponent } from "./add-athlete/add-athlete.component";
 import { EditAthleteComponent } from "./edit-athlete/edit-athlete.component";
-import { ChartComponent } from "./chart/chart.component";
 
 export const routes: Routes = [
   { path: "", component: AthletesListComponent, title: "SomatoChart" },
   { path: "new", component: AddAthleteComponent, title: "SomatoChart" },
   { path: "edit/:id", component: EditAthleteComponent, title: "SomatoChart" },
-  { path: "chart", component: ChartComponent, title: "SomatoChart" },
 ];
--- somato-chart/client/src/app/athletes-list/athletes-list.component.ts
+++ somato-chart/client/src/app/athletes-list/athletes-list.component.ts
@@ -6,6 +6,8 @@ import { MatTableModule } from "@angular/material/table";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
+import { HighchartsChartModule } from "highcharts-angular";
+import Highcharts from "highcharts";
 
 @Component({
   selector: "app-athletes-list",
@@ -18,6 +20,7 @@ import { MatIconModule } from "@angular/material/icon";
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    HighchartsChartModule,
   ],
 })
 export class AthletesListComponent implements OnInit {
@@ -46,4 +49,105 @@ export class AthletesListComponent implements OnInit {
     this.athletes$ = this.athletesService.athletes$;
     this.athletesService.getAthletes();
   }
+
+  Highcharts: typeof Highcharts = Highcharts;
+  updateFlag = false;
+
+  data = [
+    {
+      x: 1,
+      y: 2,
+      name: "test1",
+      marker: {
+        symbol: "circle",
+        fillColor: "blue",
+      },
+    },
+    {
+      x: -1,
+      y: -2,
+      name: "test2",
+      marker: {
+        symbol: "triangle",
+        fillColor: "orange",
+      },
+    },
+    {
+      x: 0,
+      y: 0,
+      name: "test3",
+      marker: {
+        symbol: "square",
+        fillColor: "purple",
+      },
+    },
+  ];
+
+  chartOptions: Highcharts.Options = {
+    chart: {
+      type: "scatter",
+      width: 750,
+      height: 750,
+      plotBackgroundImage: "assets/chart-background.svg",
+      backgroundColor: "#faf9fd",
+    },
+    title: {
+      text: "Athletes by body type",
+      align: "left",
+    },
+    xAxis: {
+      title: {
+        text: "ectomorphy - endomorphy",
+      },
+      min: -8,
+      max: 8,
+      gridLineWidth: 0,
+      tickInterval: 1,
+      tickLength: 0,
+      minorTickLength: 0,
+      lineColor: "#ccc",
+      lineWidth: 1,
+    },
+    yAxis: {
+      title: {
+        text: "2 * mesomorphy - (endomorphy + ectomorphy)",
+      },
+      min: -10,
+      max: 18,
+      gridLineWidth: 0,
+      tickInterval: 2,
+      tickLength: 0,
+      minorTickLength: 0,
+      lineColor: "#ccc",
+      lineWidth: 1,
+    },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: "#646464",
+            },
+          },
+        },
+        dataLabels: {
+          enabled: true,
+          format: "{point.name}",
+        },
+      },
+    },
+    tooltip: {
+      formatter: function () {
+        return (
+          "<b>" + this.name + "</b><br/>x: " + this.x + "<br/>y: " + this.y
+        );
+      },
+    },
+    colors: ["#646464"],
+    series: [{ type: "scatter", data: this.data }],
+  };
 }
