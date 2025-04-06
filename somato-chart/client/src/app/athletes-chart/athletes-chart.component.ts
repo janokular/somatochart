55c4ead80c1cacac518b2ef6c829eccaeb2da813
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-04-06 20:06:57 +0200

    moved chart to separate component

---
+++ somato-chart/client/src/app/athletes-chart/athletes-chart.component.ts
@@ -0,0 +1,114 @@
+import { Component } from "@angular/core";
+import { AthleteService } from "../athlete.service";
+import { MatCardModule } from "@angular/material/card";
+import { HighchartsChartModule } from "highcharts-angular";
+import Highcharts from "highcharts";
+
+@Component({
+  selector: "app-athletes-chart",
+  templateUrl: "athletes-chart.component.html",
+  styleUrls: ["athletes-chart.component.css"],
+  imports: [MatCardModule, HighchartsChartModule],
+})
+export class AthletesChartComponent {
+  somatoChart: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    chart: {
+      type: "scatter",
+      width: 750,
+      height: 750,
+      plotBackgroundImage: "assets/chart-background.svg",
+      backgroundColor: "#faf9fd",
+    },
+    title: {
+      text: "Athletes by body type",
+      align: "left",
+    },
+    xAxis: {
+      title: {
+        text: "ectomorphy - endomorphy",
+      },
+      min: -8,
+      max: 8,
+      gridLineWidth: 0,
+      tickInterval: 1,
+      tickLength: 0,
+      minorTickLength: 0,
+      lineColor: "#ccc",
+      lineWidth: 1,
+    },
+    yAxis: {
+      title: {
+        text: "2 * mesomorphy - (endomorphy + ectomorphy)",
+      },
+      min: -10,
+      max: 18,
+      gridLineWidth: 0,
+      tickInterval: 2,
+      tickLength: 0,
+      minorTickLength: 0,
+      lineColor: "#ccc",
+      lineWidth: 1,
+    },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: "#646464",
+            },
+          },
+        },
+        dataLabels: {
+          enabled: true,
+          format: "{point.name}",
+        },
+      },
+    },
+    tooltip: {
+      formatter: function () {
+        return (
+          "<b>" + this.name + "</b><br/>x: " + this.x + "<br/>y: " + this.y
+        );
+      },
+    },
+    colors: ["#646464"],
+    series: [
+      {
+        type: "scatter",
+        data: [],
+      },
+    ],
+  };
+
+  constructor(private athletesService: AthleteService) {}
+
+  ngOnInit() {
+    this.loadChartData();
+  }
+
+  private loadChartData() {
+    const chartData = this.athletesService.getAthletes().map((athlete) => ({
+      x: athlete.xAxisCoordinate,
+      y: athlete.yAxisCoordinate,
+      name: athlete.name,
+      marker: {
+        symbol: athlete.seriesSymbol,
+        fillColor: athlete.seriesColor,
+      },
+    }));
+
+    console.log(chartData);
+
+    this.chartOptions.series = [
+      {
+        type: "scatter",
+        data: chartData,
+      },
+    ];
+  }
+}
--- somato-chart/client/src/app/athletes-list/athletes-list.component.ts
+++ somato-chart/client/src/app/athletes-list/athletes-list.component.ts
@@ -12,8 +12,7 @@ import { MatTableModule } from "@angular/material/table";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
-import { HighchartsChartModule } from "highcharts-angular";
-import Highcharts from "highcharts";
+import { AthletesChartComponent } from "../athletes-chart/athletes-chart.component";
 
 @Component({
   selector: "app-athletes-list",
@@ -26,7 +25,7 @@ import Highcharts from "highcharts";
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    HighchartsChartModule,
+    AthletesChartComponent,
   ],
 })
 export class AthletesListComponent implements OnInit {
@@ -38,86 +37,11 @@ export class AthletesListComponent implements OnInit {
     "col-ecto",
     "col-action",
   ];
-  somatoChart: typeof Highcharts = Highcharts;
-  chartOptions: Highcharts.Options = {
-    chart: {
-      type: "scatter",
-      width: 750,
-      height: 750,
-      plotBackgroundImage: "assets/chart-background.svg",
-      backgroundColor: "#faf9fd",
-      animation: false,
-    },
-    title: {
-      text: "Athletes by body type",
-      align: "left",
-    },
-    xAxis: {
-      title: {
-        text: "ectomorphy - endomorphy",
-      },
-      min: -8,
-      max: 8,
-      gridLineWidth: 0,
-      tickInterval: 1,
-      tickLength: 0,
-      minorTickLength: 0,
-      lineColor: "#ccc",
-      lineWidth: 1,
-    },
-    yAxis: {
-      title: {
-        text: "2 * mesomorphy - (endomorphy + ectomorphy)",
-      },
-      min: -10,
-      max: 18,
-      gridLineWidth: 0,
-      tickInterval: 2,
-      tickLength: 0,
-      minorTickLength: 0,
-      lineColor: "#ccc",
-      lineWidth: 1,
-    },
-    legend: {
-      enabled: false,
-    },
-    plotOptions: {
-      scatter: {
-        marker: {
-          states: {
-            hover: {
-              enabled: true,
-              lineColor: "#646464",
-            },
-          },
-        },
-        dataLabels: {
-          enabled: true,
-          format: "{point.name}",
-        },
-      },
-    },
-    tooltip: {
-      formatter: function () {
-        return (
-          "<b>" + this.name + "</b><br/>x: " + this.x + "<br/>y: " + this.y
-        );
-      },
-    },
-    colors: ["#646464"],
-    series: [
-      {
-        type: "scatter",
-        data: [],
-      },
-    ],
-  };
 
   constructor(private athletesService: AthleteService) {}
 
   ngOnInit() {
     this.fetchAthletes();
-    this.loadChartData();
   }
 
   deleteAthlete(id: string): void {
@@ -129,26 +53,5 @@ export class AthletesListComponent implements OnInit {
   private fetchAthletes(): void {
     this.athletes$ = this.athletesService.athletes$;
     this.athletesService.getAthletes();
-  }
-
-  private loadChartData() {
-    const chartData = this.athletes$().map((athlete) => ({
-      x: athlete.xAxisCoordinate,
-      y: athlete.yAxisCoordinate,
-      name: athlete.name,
-      marker: {
-        symbol: athlete.seriesSymbol,
-        fillColor: athlete.seriesColor,
-      },
-    }));
-
-    console.log(chartData);
-
-    this.chartOptions.series = [
-      {
-        type: "scatter",
-        data: chartData,
-      },
-    ];
   }
 }
