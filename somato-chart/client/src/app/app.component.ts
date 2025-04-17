03d1eef01a45a89b733551953ff54849578f78ac
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-04-17 14:34:37 +0200

    staged changes

--- somato-chart/client/src/app/app.component.ts
+++ somato-chart/client/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterModule, RouterOutlet } from "@angular/router";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styleUrls: ["app.component.css"],
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule],
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class AppComponent {
   title = "client";
