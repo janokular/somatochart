fd021a87ab8ab4b6c37eaf4ecee006f16de739c1
Jan <janekokularczyk@gmail.com>
2025-09-21 12:25:30 +0200

    renamed directories

---
+++ somatochart/client/script.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function () {
+  Highcharts.chart("chart", {
+    chart: {
+      type: "scatter",
+      width: 750,
+      height: 750,
+      plotBackgroundImage: "assets/chart-background.svg",
+      backgroundColor: "#ffffff",
+    },
+    title: {
+      text: "SomatoChart",
+      style: {
+        color: "#ffffff",
+      },
+      align: "left",
+    },
+    xAxis: {
+      title: {
+        text: "ectomorphy - endomorphy",
+      },
+      min: -8,
+      max: 8,
+      gridLineWidth: 0,
+      tickInterval: 1,
+      tickLength: 0,
+      minorTickLength: 0,
+      lineColor: "#ccc",
+      lineWidth: 1,
+    },
+    yAxis: {
+      title: {
+        text: "2 * mesomorphy - (endomorphy + ectomorphy)",
+      },
+      min: -10,
+      max: 18,
+      gridLineWidth: 0,
+      tickInterval: 2,
+      tickLength: 0,
+      minorTickLength: 0,
+      lineColor: "#ccc",
+      lineWidth: 1,
+    },
+    legend: {
+      enabled: false,
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      formatter: function () {
+        return (
+          "<b>" + this.name + "</b><br/>x: " + this.x + "<br/>y: " + this.y
+        );
+      },
+    },
+    colors: ["transparent"],
+    exporting: {
+      buttons: {
+        contextButton: {
+          symbolStroke: "#000000",
+          menuItems: ["downloadPNG", "downloadJPEG", "downloadSVG"],
+        },
+      },
+      chartOptions: {
+        chart: {
+          plotBackgroundImage: "assets/chart-background.jpg",
+          backgroundColor: "#ffffff",
+        },
+      },
+    },
+    series: [
+      {
+        type: "scatter",
+        data: [
+          { x: 1.3, y: 2.3, symbol: "circle", color: "blue" },
+          { x: -2.5, y: -4.5, symbol: "circle", color: "blue" },
+          { x: 2.2, y: 3, symbol: "circle", color: "blue" },
+        ],
+      },
+    ],
+  });
+});
