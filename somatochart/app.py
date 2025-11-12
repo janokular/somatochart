dae9d84e00cee9846b60693b41ab21a2bdc9fa5f
janokular <janekokularczyk@gmail.com>
2025-11-12 12:00:52 +0100

    updated app.py

--- somatochart/app.py
+++ somatochart/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
 client = MongoClient(mongo_uri)
 db = client['somatochart']
-athletes = db['athletes']
+athletes_collection = db['athletes']
 
 REQUIRED_FIELDS = ['endo', 'meso', 'ecto', 'name', 'color', 'symbol', 'isVisible']
 
@@ -25,10 +25,25 @@ def index_page():
 
 @app.route('/athletes', methods=['GET'])
 def get_athletes():
-    docs = list(athletes.find({}))
-    for d in docs:
-        d['_id'] = str(d['_id'])
-    return jsonify(docs)
+    try:
+        athletes = list(athletes_collection.find({}))
+        if athletes:
+            for d in athletes:
+                d['_id'] = str(d['_id'])
+            return jsonify(athletes), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@app.route('/athletes/<id>', methods=['GET'])
+def get_athlete(id):
+    try:
+        athlete = athletes_collection.find_one({'_id': ObjectId(id)})
+        if athlete:
+            athlete['_id'] = str(athlete['_id'])
+            return jsonify(athlete), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 
 @app.route('/athletes', methods=['POST'])
@@ -47,7 +62,7 @@ def add_athlete():
                           data['symbol'],
                           data['isVisible'])
         
-        athletes.insert_one(athlete.to_dict())
+        athletes_collection.insert_one(athlete.to_dict())
         return jsonify({'message': 'Athlete added successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400
@@ -69,7 +84,7 @@ def update_athlete(id):
                           data['symbol'],
                           data['isVisible'])
 
-        result = athletes.update_one({'_id': ObjectId(id)}, {'$set': athlete.to_dict()})
+        result = athletes_collection.update_one({'_id': ObjectId(id)}, {'$set': athlete.to_dict()})
         if result.modified_count > 0:
             return jsonify({'message': 'Athlete updated successfully'}), 200
         return jsonify({'message': 'No changes'}), 404
@@ -80,7 +95,7 @@ def update_athlete(id):
 @app.route('/athletes/<id>', methods=['DELETE'])
 def delete_athlete(id):
     try:
-        result = athletes.delete_one({'_id': ObjectId(id)})
+        result = athletes_collection.delete_one({'_id': ObjectId(id)})
         if result.deleted_count > 0:
             return jsonify({'message': 'Athlete deleted successfully'}), 200
         return jsonify({'message': 'Athlete not found'}), 404
