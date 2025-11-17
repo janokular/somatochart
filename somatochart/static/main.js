f4f3b658d9aecd900ae0a7582478550b40fbdf13
janokular <janekokularczyk@gmail.com>
2025-11-17 14:03:50 +0100

    updated main.js

--- somatochart/static/main.js
+++ somatochart/static/main.js
@@ -1,8 +1,8 @@
-function loadChartAndList() {
+function loadData() {
   fetch("/athletes")
     .then((res) => res.json())
     .then((athletes) => {
-      Highcharts.chart("chart", {
+      const chart = Highcharts.chart("chart", {
         chart: {
           type: "scatter",
           width: 750,
@@ -15,9 +15,6 @@ function loadChartAndList() {
         },
         title: {
           text: "SomatoChart",
-          style: {
-            color: "#ffffff",
-          },
           align: "left",
         },
         xAxis: [{
@@ -64,16 +61,15 @@ function loadChartAndList() {
         },
         colors: ["transparent"],
         exporting: {
+          enabled: true,
           buttons: {
             contextButton: {
-              symbolStroke: "#000000",
-              menuItems: ["downloadPNG", "downloadJPEG", "downloadSVG"],
+              enabled: false,
             },
           },
           chartOptions: {
             chart: {
               plotBackgroundImage: "static/assets/chart-background.jpg",
-              backgroundColor: "#ffffff",
             },
           },
         },
@@ -104,90 +100,109 @@ function loadChartAndList() {
       list.innerHTML = "";
       athletes.forEach(a => {
         const li = document.createElement("li");
-        li.textContent = `${a.name} ${a.endo} ${a.meso} ${a.ecto} ${a.color} ${a.symbol} ${a.isVisible}`;
+        li.textContent = `${a.name} endo: ${a.endo} meso: ${a.meso} ecto: ${a.ecto} ${a.color} ${a.symbol} ${a.isVisible}`;
 
-        const updateBtn = document.createElement("button");
-        updateBtn.textContent = "Update";
-        updateBtn.addEventListener("click", () => {
-          console.log(`updated ${a._id}`);
-        });
+        // const updateBtn = document.createElement("button");
+        // updateBtn.textContent = "Update";
+        // updateBtn.addEventListener("click", () => {
+        //   console.log(`updated ${a._id}`);
+        // });
 
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "Delete";
         deleteBtn.addEventListener("click", () => {
           fetch(`/athletes/${a._id}`, { method: "DELETE" })
             .then((res) => res.json())
-            .then(() => loadChartAndList());
+            .then(() => loadData());
         });
 
-        li.appendChild(updateBtn);
+        // li.appendChild(updateBtn);
         li.appendChild(deleteBtn);
         list.appendChild(li);
       });
+
+      document
+        .getElementById("download")
+        .addEventListener("click", () => {   
+          chart.exportChart({
+            type: 'image/png',
+            filename: 'somatochart',
+          });
+        });
     });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  loadChartAndList();
+  loadData();
 
   document
-    .getElementById("add-form")
+    .getElementById("addForm")
     .addEventListener("submit", function (e) {
       e.preventDefault();
+      const name = e.target.name.value;
       const endo = e.target.endo.value;
       const meso = e.target.meso.value;
       const ecto = e.target.ecto.value;
-      const name = e.target.name.value;
       const color = e.target.color.value;
       const symbol = e.target.symbol.value;
-      var isVisible = document.getElementById("visibilityToggle").checked ? true : false;
+      const isVisible = e.target.isVisible.value === "true";
       e.target.reset();
 
       fetch("/athletes", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
+          name,
           endo: Number(endo),
           meso: Number(meso),
           ecto: Number(ecto),
-          name,
           color,
           symbol,
           isVisible,
         }),
       })
         .then((res) => res.json())
-        .then(() => loadChartAndList());
+        .then(() => loadData());
     });
 
+  // document
+  //   .getElementById("updateForm")
+  //   .addEventListener("submit", function (e) {
+  //     e.preventDefault();
+  //     const id = e.target.id.value;
+  //     const name = e.target.name.value;
+  //     const endo = e.target.endo.value;
+  //     const meso = e.target.meso.value;
+  //     const ecto = e.target.ecto.value;
+  //     const color = e.target.color.value;
+  //     const symbol = e.target.symbol.value;
+  //     const isVisible = (e.target.isVisible.value === "true");
+  //     e.target.reset();
+
+  //     fetch(`/athletes/${id}`, {
+  //       method: "PUT",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         name,
+  //         endo: Number(endo),
+  //         meso: Number(meso),
+  //         ecto: Number(ecto),
+  //         color,
+  //         symbol,
+  //         isVisible,
+  //       }),
+  //     })
+  //       .then((res) => res.json())
+  //       .then(() => loadChartAndList());
+  //   });
+  
   document
-    .getElementById("update-form")
-    .addEventListener("submit", function (e) {
-      e.preventDefault();
-      const id = e.target.id.value;
-      const endo = e.target.endo.value;
-      const meso = e.target.meso.value;
-      const ecto = e.target.ecto.value;
-      const name = e.target.name.value;
-      const color = e.target.color.value;
-      const symbol = e.target.symbol.value;
-      var isVisible = document.getElementById("updateVisibilityToggle").checked ? true : false;
-      e.target.reset();
-
-      fetch(`/athletes/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          endo: Number(endo),
-          meso: Number(meso),
-          ecto: Number(ecto),
-          name,
-          color,
-          symbol,
-          isVisible,
-        }),
+    .getElementById("clear")
+    .addEventListener("click", () => {
+      fetch('/athletes', {
+        method: "DELETE"
       })
         .then((res) => res.json())
-        .then(() => loadChartAndList());
-    });
+        .then(() => loadData());
+  });
 });
