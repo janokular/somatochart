cdbf250e787bc9a570667bfe40b0fa18ad6d521c
janokular <janekokularczyk@gmail.com>
2025-11-21 12:19:47 +0100

    updated __init__.py

--- somatochart/app/__init__.py
+++ somatochart/app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from pymongo import MongoClient
 from app.config import Config
 from app.routes import routes
 
@@ -7,10 +6,6 @@ from app.routes import routes
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    client = MongoClient(app.config['MONGO_URI'])
-    db = client.get_database()
-    app.athletes_collection = db["athletes"]
-    
     app.register_blueprint(routes)
+    
     return app
