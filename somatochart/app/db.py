bde26951d37aabf896c079ab00e41665e1764925
janokular <janekokularczyk@gmail.com>
2025-11-21 15:45:50 +0100

    updated db.py

--- somatochart/app/db.py
+++ somatochart/app/db.py
@@ -2,5 +2,11 @@ from pymongo import MongoClient
 from app.config import Config
 
 
-client = MongoClient(Config.MONGO_URI)
-db = client.get_database()
+def get_db():
+    client = MongoClient(Config.MONGO_URI)
+    return client.get_database()
+
+
+def get_collection(name):
+    db = get_db()
+    return db[name] 
