69109122b7d2238df5b0b6870f40ffee40708c28
janokular <janekokularczyk@gmail.com>
2026-05-11 11:52:49 +0200

    staged changes

--- somatochart/csv_parser.py
+++ somatochart/csv_parser.py
@@ -7,28 +7,28 @@ from .models import Athlete
 
 def csv_parser(csv_file) -> list[dict]:
     '''
-    Read CSV file, parse data using Athlete schema and
-    return list of dictionaires with Athletes data
+    Read CSV file, parse data to expected type based on Athlete schema
+    and return list of dictionaires with Athletes data
     '''
-    athletes = []
-    
     stream = TextIOWrapper(
         csv_file.stream,
         encoding='utf-8',
         newline=''
     )
-
     reader = DictReader(stream)
+
+    athletes = []
+    expected_type = get_type_hints(Athlete)
 
     for row in reader:
         parsed_row = {}
         for key, value in row.items():
-            expected_type = get_type_hints(Athlete).get(key)
             try:
-                parsed_row[key] = expected_type(value)
+                parsed_row[key] = expected_type[key](value)
             except:
-                raise Exception(f'Inccorect data type {key} is expected to be {expected_type}')
-        athlete = Athlete(**parsed_row).to_dict()
-        athletes.append(athlete)
+                raise Exception(
+                    f"Inccorect data type, {key} is of type {expected_type[key].__name__} but found value '{value}'"
+                )
+        athletes.append(Athlete(**parsed_row).to_dict())
 
     return athletes
